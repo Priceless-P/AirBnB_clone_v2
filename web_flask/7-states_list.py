17c593426860ea50b6c7427454cff6dedca7d683
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2024-01-28 13:14:17 +0100

    debug task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,88 +2,29 @@
 """
 Script that starts a Flask web application:
 - Routes:
-   - /: display “Hello HBNB!”
-  /hbnb: display “HBNB”
-  /c/<text>: display “C ”, followed by the value of the text
-  /python/<text>: display “Python ”, followed by the value of the text
-  /number/<n>: display “n is a number” only if n is an integer
-  /number_template/<n>: display a HTML page only if n is an integer
-  /number_odd_or_even/<n>: display a HTML page only if n is an integer:
-        H1 tag: “Number: n is even|odd
   /states_list: display a HTML page with listen of states sorted by state name
 """
 from flask import render_template
-from markupsafe import escape
 from flask import Flask
-import models
+from models import storage
 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route("/")
-def hello_hbnb():
-    """Displays Hello HBNB"""
-    return "Hello HBNB!"
-
-
-@app.route("/hbnb")
-def hbnb():
-    """Displays HBNB"""
-    return "HBNB!"
-
-
-@app.route("/c/<text>")
-def text(text):
-    """Displays C followed by text"""
-    return "C {}".format(escape(text.replace('_', ' ')))
-
-
-@app.route("/python")
-@app.route("/python/<text>")
-def python(text="is cool"):
-    """Displays Python followed by text"""
-    return "Python {}".format(escape(text.replace('_', ' ')))
-
-
-@app.route("/number/<int:n>")
-def number(n):
-    """Displays n only if it is an integer"""
-    return "{} is a number".format(escape(n))
-
-
-@app.route("/number_template/<int:n>")
-def number_template(n):
-    """Displays n in template only if it is an integer"""
-    return render_template('5-number.html', number=n)
-
-
-@app.route("/number_odd_or_even/<int:n>")
-def number_odd_even(n):
-    """Displays n and even or odd
-    if number is integer and result is either
-    """
-    if n % 2 == 0:
-        result = "even"
-    else:
-        result = "odd"
-    return render_template("6-number_odd_or_even.html",
-                           number=n, result=result)
-
-
 @app.route("/states_list")
 def states():
     """Displays all sates"""
     from models import State
-    states = models.storage.all(State).values()
+    states = sorted(storage.all(State).values(), key=lambda state: state.name)
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
 def tear_down(exception):
     """Removes current SQLAlchemy Session"""
-    models.storage.close()
+    storage.close()
 
 
 if __name__ == "__main__":
